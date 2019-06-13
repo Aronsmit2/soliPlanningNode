65e56cf57f140ec8c7c721323fd53066bf364fb6
Aron Smit <aronsmit@iMac-van-Aron.local>
2019-06-13 11:23:01 +0200

    setup navigation and footer | setup login page

--- soliPlanningNode/routes/index.js
+++ soliPlanningNode/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Soli planning' });
 });
 
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Soli planning' });
+});
 module.exports = router;
